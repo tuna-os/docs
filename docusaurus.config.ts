8cc62a17590b0a993c00d22a7ddcdf1062a1a2f7
James Reilly <jreilly1821@gmail.com>
2026-06-22 21:28:37 +0530

    feat: use tuna-os org avatar as site logo and favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
       title: 'TunaOS',
       logo: {
         alt: 'TunaOS Logo',
-        src: 'img/tuna-logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
